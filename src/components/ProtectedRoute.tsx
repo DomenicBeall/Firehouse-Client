e7fe7619f5f66fedb76a5188688fa3708bf6d574
Domenic Beall <domenic.beall@aroflo.com>
2022-06-05 11:39:36 +1000

    Added comments to protected route

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,6 +1,12 @@
+// This component wraps a normal Route component
+// It checks if the user is loaded and if they aren't redirects them to the login page
+
+// Import dependency's
 import { useAuth0 } from "@auth0/auth0-react";
 import { Redirect, Route } from 'react-router-dom';
+import Loading from "../pages/Loading";
 
+// Define the structure of the props
 type ProtectedRouteProps = {
     path: string,
     exact: boolean,
@@ -8,22 +14,26 @@ type ProtectedRouteProps = {
 };
 
 const ProtectedRoute = (props: ProtectedRouteProps) => {
-    const { loginWithRedirect } = useAuth0();
-    const { user, isAuthenticated, isLoading } = useAuth0();
+    // Get relevant functions and values from Auth0
+    const { loginWithRedirect, isAuthenticated, isLoading } = useAuth0();
 
+    // If the user is still loading, show the loading screen
     if (isLoading) {
-        return <div>Loading...</div>
+        return <Loading/>;
     }
 
     if (isAuthenticated) {
+        // If the user is authenticated route them to where they want to go
+
         return (
-            <Route path={props.path}>
+            <Route exact={props.exact} path={props.path}>
                 {props.children}
             </Route>
         );
     } else {
+        // If the user isn't authenticated redirect them to Auth0 login
         loginWithRedirect();
-        return <div>Loading...</div>;
+        return <Loading/>;
     }
 };
 
